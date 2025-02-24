94411b713ea3030e79a072db5bb96709733ecfa8
simon <simonpham1294@gmail.com>
2025-02-24 14:35:34 -0400

    update server

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,9 @@
 const Pool = require('pg').Pool;
 const pool = new Pool({
-  user: 'postgres',
-  password: 'Hanzo1412.',
+  user: 'simon_database_user',
+  password: 'NunnHAAIQqYu9y6N3lcpK1hzSR8Cn5cR',
   port: 5432,
-  database: 'perntodo'
+  database: 'simon_database'
 })
 
 module.exports = pool;
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const cors = require('cors');
-const pool = require('./db');
+// const cors = require('cors');
+// const pool = require('./db');
 const SQL = require('sql-template-strings');
+const port = process.env.PORT || 5000;
 
 const app = express();
 
