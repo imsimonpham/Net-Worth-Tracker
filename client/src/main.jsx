15dd96c27e107f90e217b4ab76708694f27dea97
simon <simonpham1294@gmail.com>
2025-04-21 21:34:08 -0300

    update title

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,13 +4,9 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
-  // <StrictMode>
-  //   <BrowserRouter>
-  //     <App />
-  //   </BrowserRouter>
-  // </StrictMode>
-
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 )
